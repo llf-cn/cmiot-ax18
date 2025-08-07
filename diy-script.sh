fd4eb16ca8b80dd8af84d8bb5051b17e419f1212
llf-cn <61875916+llf-cn@users.noreply.github.com>
2025-08-08 02:04:16 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -7,3 +7,10 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 # 不拉取或构建 WiFi 驱动（保险措施）
 sed -i '/kmod-ath11k/d' feeds.conf.default || true
 sed -i '/kmod-mac80211/d' feeds.conf.default || true
+#!/bin/bash
+
+# 添加 OpenClash 源
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
+
+./scripts/feeds update -a
+./scripts/feeds install -a
