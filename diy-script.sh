6a9cea1f1861dfea16e6008774fc9e65dd0c93d9
llf-cn <61875916+llf-cn@users.noreply.github.com>
2025-08-08 10:57:00 +0800

    1

--- diy-script.sh
+++ diy-script.sh
@@ -4,11 +4,6 @@ set -eux
 # 修改默认 LAN IP 为 192.168.0.1
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# 不拉取或构建 WiFi 驱动（保险措施）
-sed -i '/kmod-ath11k/d' feeds.conf.default || true
-sed -i '/kmod-mac80211/d' feeds.conf.default || true
-#!/bin/bash
-
 # 添加 OpenClash 源
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
 
